103d0f8db12160d68944c1024fec9a726c296c07
Hanna Linn <hanna@moviepilot.com>
2013-05-04 15:03:20 +0200

    generate a new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsgirls::Application.config.secret_token = '85330446bd42ce74e7acd4c24f1727567964db0a0afdfe7fdb5e46e263821ed37f4ee79798cc317eef5d6f5638a471d8edca1a56f75f5c69ba274c11a6d78ecb'
